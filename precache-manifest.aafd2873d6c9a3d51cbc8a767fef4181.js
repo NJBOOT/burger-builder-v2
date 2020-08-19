adc84d8cdb4f503c7e71132a445ba762afaeaa39
Nico Barbarese <nicobarbarese@gmail.com>
2020-08-19 12:59:56 -0400

    Updates

---
+++ precache-manifest.aafd2873d6c9a3d51cbc8a767fef4181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05f0fe5802d5dd81fb2805194d3f529",
+    "url": "/burger-builder-v2/index.html"
+  },
+  {
+    "revision": "54bce9c67572e1cec0a9",
+    "url": "/burger-builder-v2/static/css/main.5893fb26.chunk.css"
+  },
+  {
+    "revision": "93598e8c97205e59d523",
+    "url": "/burger-builder-v2/static/js/2.a4001fff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder-v2/static/js/2.a4001fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54bce9c67572e1cec0a9",
+    "url": "/burger-builder-v2/static/js/main.8fab6642.chunk.js"
+  },
+  {
+    "revision": "12be4e40904dfdcfd4fa",
+    "url": "/burger-builder-v2/static/js/runtime-main.1184d206.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-v2/static/media/burger-logo.b8503d26.png"
+  }
+]);
